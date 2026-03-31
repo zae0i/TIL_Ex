a3477c67fe3014f108aeca2a9ca8ff23b2a39614
박재영 <pjyng115@naver.com>
2026-03-31 16:30:25 +0900

    Week1 HelloWorld 예제 추가

---
+++ Week1/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("이것은 주차별 예제 프로젝트의 Week1 HelloWorld 코드입니다.");
+    }
+}
